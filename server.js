b0b0e5ce0b6daa8b0385019463ad294e65c2a2a4
Kim Maida <kim.maida@auth0.com>
2017-04-28 15:54:08 -0400

    Add MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-// Module dependencies
+/*
+ |--------------------------------------------------------------------------
+ | Dependencies
+ |--------------------------------------------------------------------------
+ */
+
+// Modules
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -8,17 +14,48 @@ const methodOverride = require('method-override');
 // Config
 const config = require('./server/config');
 
-// App
+/*
+ |--------------------------------------------------------------------------
+ | MongoDB
+ |--------------------------------------------------------------------------
+ */
+
+mongoose.connect(config.MONGO_URI);
+const monDb = mongoose.connection;
+
+monDb.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that', config.MONGO_URI, 'is running.');
+});
+
+monDb.once('open', function callback() {
+  console.info('Connected to MongoDB:', config.MONGO_URI);
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | App
+ |--------------------------------------------------------------------------
+ */
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+// Set port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
 // Set static path to Angular app in dist
 app.use(express.static(path.join(__dirname, './dist')));
 
-// API routes
+/*
+ |--------------------------------------------------------------------------
+ | Routes
+ |--------------------------------------------------------------------------
+ */
+
 require('./server/api')(app, config);
 
 // Pass routing to Angular app
@@ -26,9 +63,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Set port
-const port = process.env.PORT || '3000';
-app.set('port', port);
+/*
+ |--------------------------------------------------------------------------
+ | Server
+ |--------------------------------------------------------------------------
+ */
 
-// Listen
-app.listen(port, () => console.log(`API running on localhost:${port}`));
+app.listen(port, () => console.log(`App running on localhost:${port}`));
