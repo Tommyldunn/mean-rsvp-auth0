398edd40f07d904518ccc4975dfe5be10306cc20
Kim Maida <kim.maida@auth0.com>
2017-05-01 14:12:27 -0400

    Fix and clean up navigation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   navOpen: boolean;
-  minHeight: string;
-  private initWinHeight = 0;
-
-  ngOnInit() {
-    Observable.fromEvent(window, 'resize')
-      .debounceTime(200)
-      .subscribe((event) => this.resizeFn(event));
-
-    this.initWinHeight = window.innerHeight;
-    this.resizeFn(null);
-  }
 
   navToggleHandler(e: boolean) {
     this.navOpen = e;
-  }
-
-  private resizeFn(e) {
-    const winHeight: number = e ? e.target.innerHeight : this.initWinHeight;
-    this.minHeight = `${winHeight}px`;
   }
 }
