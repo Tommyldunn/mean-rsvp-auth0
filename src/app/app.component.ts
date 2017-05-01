ff4dc36ff4c33289bcd6244d1868a103e237c017
Kim Maida <kim.maida@auth0.com>
2017-05-01 13:31:13 -0400

    add components and oc nav. BUG: extra whitespace in webkit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  navOpen: boolean;
+  minHeight: string;
+  private initWinHeight = 0;
+
+  ngOnInit() {
+    Observable.fromEvent(window, 'resize')
+      .debounceTime(200)
+      .subscribe((event) => this.resizeFn(event));
+
+    this.initWinHeight = window.innerHeight;
+    this.resizeFn(null);
+  }
+
+  navToggleHandler(e: boolean) {
+    this.navOpen = e;
+  }
+
+  private resizeFn(e) {
+    const winHeight: number = e ? e.target.innerHeight : this.initWinHeight;
+    this.minHeight = `${winHeight}px`;
+  }
 }
