1a10ac2d8952a17393c9cda62b7e5efa6a86b810
Kim Maida <kim.maida@auth0.com>
2017-05-24 17:33:10 -0400

    For consistency, use backticks in all validation messages.

--- src/app/pages/admin/event-form/event-form.service.ts
+++ src/app/pages/admin/event-form/event-form.service.ts
@@ -25,39 +25,39 @@ export class EventFormService {
   constructor() {
     this.validationMessages = {
       title: {
-        required: 'Title is <strong>required</strong>.',
+        required: `Title is <strong>required</strong>.`,
         minlength: `Title must be ${this.textMin} characters or more.`,
         maxlength: `Title must be ${this.titleMax} characters or less.`
       },
       location: {
-        required: 'Location is <strong>required</strong>.',
+        required: `Location is <strong>required</strong>.`,
         minlength: `Location must be ${this.textMin} characters or more.`,
         maxlength: `Location must be ${this.locMax} characters or less.`
       },
       startDate: {
-        required: 'Start date is <strong>required</strong>.',
+        required: `Start date is <strong>required</strong>.`,
         maxlength: `Start date cannot be longer than ${this.dateMax} characters.`,
-        pattern: 'Start date must be in the format <strong>M/D/YYYY</strong> or <strong>MM/DD/YYYY</strong>.',
-        date: 'Start date must be a <strong>valid date</strong> at least one day <strong>in the future</strong>.'
+        pattern: `Start date must be in the format <strong>M/D/YYYY</strong> or <strong>MM/DD/YYYY</strong>.`,
+        date: `Start date must be a <strong>valid date</strong> at least one day <strong>in the future</strong>.`
       },
       startTime: {
-        required: 'Start time is <strong>required</strong>.',
-        pattern: 'Start time must be a <strong>valid time</strong> in the format <strong>HH:MM AM/PM</strong>.',
+        required: `Start time is <strong>required</strong>.`,
+        pattern: `Start time must be a <strong>valid time</strong> in the format <strong>HH:MM AM/PM</strong>.`,
         maxlength: `Start time must be ${this.timeMax} characters or less.`
       },
       endDate: {
-        required: 'End date is <strong>required</strong>.',
+        required: `End date is <strong>required</strong>.`,
         maxlength: `End date cannot be longer than ${this.dateMax} characters.`,
-        pattern: 'End date must be in the format <strong>M/D/YYYY</strong> or <strong>MM/DD/YYYY</strong>.',
-        date: 'End date must be a <strong>valid date</strong> at least one day <strong>in the future</strong>.'
+        pattern: `End date must be in the format <strong>M/D/YYYY</strong> or <strong>MM/DD/YYYY</strong>.`,
+        date: `End date must be a <strong>valid date</strong> at least one day <strong>in the future</strong>.`
       },
       endTime: {
-        required: 'End time is <strong>required</strong>.',
-        pattern: 'End time must be a <strong>valid time</strong> in the format <strong>HH:MM AM/PM</strong>.',
+        required: `End time is <strong>required</strong>.`,
+        pattern: `End time must be a <strong>valid time</strong> in the format <strong>HH:MM AM/PM</strong>.`,
         maxlength: `End time must be ${this.timeMax} characters or less.`
       },
       viewPublic: {
-        required: 'You must specify whether this event should be publicly listed.'
+        required: `You must specify whether this event should be publicly listed.`
       },
       description: {
         maxlength: `Description must be ${this.descMax} characters or less.`
