00ef4e6a17ac137c58e3dc4da7c2425046c4cc98
Kim Maida <kim.maida@auth0.com>
2017-05-25 16:59:23 -0400

    Remove unused import from event form.

--- src/app/pages/admin/event-form/event-form.component.ts
+++ src/app/pages/admin/event-form/event-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { ApiService } from './../../../core/api.service';
