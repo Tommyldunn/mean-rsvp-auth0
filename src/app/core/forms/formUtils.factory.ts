d15eb4cbafb874a33842dd096d79b7365e815dcb
Kim Maida <kim.maida@auth0.com>
2017-05-25 11:10:49 -0400

    Add more thorough regex commenting to formUtils factory.

--- src/app/core/forms/formUtils.factory.ts
+++ src/app/core/forms/formUtils.factory.ts
@@ -1,13 +1,22 @@
 // 0-9
+// https://regex101.com/r/dU0eY6/1
 const guestsRegex = new RegExp(/^[0-9]$/);
-// MM/DD/YYYY, M/D/YYYY
-const dateRegex = new RegExp(/^(\d{2}|\d{1})\/(\d{2}|\d{1})\/\d{4}$/);
-// HH:MM am/pm, HH:MM AM/PM
-const timeRegex = new RegExp(/\b((1[0-2]|0?[1-9]):([0-5][0-9]) ([AaPp][Mm]))$/);
+// mm/dd/yyyy, m/d/yyyy
+// https://regex101.com/r/7iSsmm/2
+const dateRegex = new RegExp(/^(\d{2}|\d)\/(\d{2}|\d)\/\d{4}$/);
+// h:mm am/pm, hh:mm AM/PM
+// https://regex101.com/r/j2Cfqd/1/
+const timeRegex = new RegExp(/^((1[0-2]|0?[1-9]):([0-5][0-9]) ([AaPp][Mm]))$/);
 
-// Convert date and time strings to a Date object
+// Converts date + time strings to a Date object.
+// Date and time parameters should have already
+// been validated with dateRegex and timeRegex.
 function stringsToDate(dateStr: string, timeStr: string) {
-  const timeArr = timeStr.split(/[\s:]+/);
+  if (!dateRegex.test(dateStr) || !timeRegex.test(timeStr)) {
+    console.error('Cannot convert date/time to Date object.');
+    return;
+  }
+  const timeArr = timeStr.split(/[\s:]+/); // https://regex101.com/r/H4dMvA/1
   const date = new Date(dateStr);
   let hour = parseInt(timeArr[0], 10);
   const min = parseInt(timeArr[1], 10);
@@ -21,7 +30,6 @@ function stringsToDate(dateStr: string, timeStr: string) {
   }
   date.setHours(hour);
   date.setMinutes(min);
-
   return date;
 }
 
