f8130e38dac503840111d632fe78bb47df5f5d87
Kim Maida <kim.maida@auth0.com>
2017-06-02 16:42:11 -0400

    Clean up date range validator.

--- src/app/core/forms/date-range.validator.ts
+++ src/app/core/forms/date-range.validator.ts
@@ -9,17 +9,14 @@ export function dateRangeValidator(c: AbstractControl) {
   const endTimeC = c.get('endTime');
   // Object to return if date is invalid
   const invalidObj = { 'dateRange': true };
-  let startDatetime;
-  let endDatetime;
 
   // If start and end dates are valid, can check range (with prefilled times)
   // Final check happens when all dates/times are valid
   if (startDateC.valid && endDateC.valid) {
     const checkStartTime = startTimeC.invalid ? '12:00 AM' : startTimeC.value;
     const checkEndTime = endTimeC.invalid ? '11:59 PM' : endTimeC.value;
-
-    startDatetime = stringsToDate(startDateC.value, checkStartTime);
-    endDatetime = stringsToDate(endDateC.value, checkEndTime);
+    const startDatetime = stringsToDate(startDateC.value, checkStartTime);
+    const endDatetime = stringsToDate(endDateC.value, checkEndTime);
 
     if (endDatetime >= startDatetime) {
       return null;
