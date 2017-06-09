0db82e3c07c476132950740f987920fbb3dc30d8
Kim Maida <kim.maida@auth0.com>
2017-06-09 12:11:23 -0400

    Clean up app-routing module to remove unnecessary imports.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,7 @@ import { AdminGuard } from './auth/admin.guard';
 // Page components
 import { HomeComponent } from './pages/home/home.component';
 import { CallbackComponent } from './pages/callback/callback.component';
-import { EventComponent } from './pages/event/event.component';
 import { MyRsvpsComponent } from './pages/my-rsvps/my-rsvps.component';
-import { AdminComponent } from './pages/admin/admin.component';
-import { CreateEventComponent } from './pages/admin/create-event/create-event.component';
-import { UpdateEventComponent } from './pages/admin/update-event/update-event.component';
 
 const routes: Routes = [
   {
